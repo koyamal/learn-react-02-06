ee14ef17bc8531640ba594b079fbaee28de66231
koyamal <74910401+koyamal@users.noreply.github.com>
2022-11-08 08:24:14 +0900

    Edit tsconfig.json

---
+++ src/practices/Practice4.tsx
@@ -0,0 +1,15 @@
+export const Practice4 = () => {
+  const calcTotalFee = (num: number) => {
+    const total = num * 1.1;
+    console.log(total);
+  };
+  const onClickPractice = () => {
+    calcTotalFee(1000);
+  };
+  return (
+    <div>
+      <p>Practice4: Edit Config</p>
+      <button onClick={onClickPractice}>Practice4</button>
+    </div>
+  );
+};
